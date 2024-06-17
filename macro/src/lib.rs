54520c2b95ce78542ff9c5f6cfc8ec3d66c806c4
u-kai <76635578+u-kai@users.noreply.github.com>
2024-06-17 18:31:25 +0900

    add example

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -7,20 +7,45 @@ use syn::{
     Ident, Type,
 };
 
-fn dependencies() -> proc_macro2::TokenStream {
-    quote! {
-        use pte::{
-            Lines,
-        };
-    }
-}
-
 #[proc_macro_attribute]
 pub fn pte(
     attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
     pte_impl(attr.into(), item.into()).into()
+}
+
+fn pte_impl(
+    attr: proc_macro2::TokenStream,
+    item: proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
+    let dependencies = dependencies();
+    let fn_sig = fn_parse.parse2(item).unwrap();
+    let attr = attr.to_string();
+    let parser = PteAttrParser::new(&attr);
+    let consume_lines = consume_lines(&fn_sig, parser);
+    let fn_sig_declare = fn_declare(&fn_sig);
+    let fn_sig_execute = fn_execute(&fn_sig);
+
+    quote! {
+        #dependencies
+
+        #fn_sig_declare
+
+        fn main() {
+            #consume_lines
+            let result = #fn_sig_execute
+            println!("{}", result);
+        }
+    }
+}
+
+fn dependencies() -> proc_macro2::TokenStream {
+    quote! {
+        use pte::{
+            Lines,
+        };
+    }
 }
 
 fn fn_execute(fn_sig: &FunctionSignature) -> proc_macro2::TokenStream {
@@ -47,6 +72,155 @@ fn fn_declare(fn_sig: &FunctionSignature) -> proc_macro2::TokenStream {
     }
 }
 
+fn consume_lines(
+    fn_sig: &FunctionSignature,
+    parse_attr: PteAttrParser,
+) -> proc_macro2::TokenStream {
+    if parse_attr.exist_row_num_at_input() {
+        let input_ref = parse_attr.get_input_ref().unwrap();
+        return consume_lines_from_input(fn_sig, input_ref);
+    }
+    if parse_attr.exist_row_num() {
+        let n = parse_attr.get_row_num().unwrap();
+        return consume_lines_from_row_num(fn_sig, n as usize);
+    }
+    if parse_attr.exist_row_num_at_var_name() {
+        let var_name = parse_attr.get_var_name().unwrap();
+        return consume_lines_from_var_name(fn_sig, var_name);
+    }
+    default_consume_lines(fn_sig)
+}
+
+fn default_consume_lines(fn_sig: &FunctionSignature) -> proc_macro2::TokenStream {
+    let result = fn_sig
+        .args()
+        .iter()
+        .map(|(name, ty)| {
+            arg_to_consume_line_token_stream(
+                name,
+                ty,
+                &syn::Ident::new("lines", fn_sig.name.span()),
+            )
+        })
+        .collect::<Vec<_>>();
+    quote! {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let mut lines = Lines::new(&input);
+
+        #(#result)*
+    }
+}
+
+fn consume_lines_from_input(
+    fn_sig: &FunctionSignature,
+    input_num: usize,
+) -> proc_macro2::TokenStream {
+    let result = fn_sig
+        .args()
+        .iter()
+        .map(|(name, ty)| {
+            arg_to_consume_line_token_stream(
+                name,
+                ty,
+                &syn::Ident::new("lines", fn_sig.name.span()),
+            )
+        })
+        .collect::<Vec<_>>();
+    let input_ref = proc_macro2::Literal::usize_unsuffixed(input_num);
+
+    quote! {
+        let mut first_line = String::new();
+        std::io::stdin().read_line(&mut first_line).unwrap();
+
+        let row_num = first_line.split_whitespace().nth(#input_ref).unwrap().parse::<usize>().unwrap();
+
+        let mut input = String::new();
+        for _ in 0..row_num {
+            std::io::stdin().read_line(&mut input).unwrap();
+        }
+        let mut lines = Lines::new(&input);
+        #(#result)*
+    }
+}
+fn consume_lines_from_row_num(fn_sig: &FunctionSignature, n: usize) -> proc_macro2::TokenStream {
+    let result = fn_sig
+        .args()
+        .iter()
+        .map(|(name, ty)| {
+            arg_to_consume_line_token_stream(
+                name,
+                ty,
+                &syn::Ident::new("lines", fn_sig.name.span()),
+            )
+        })
+        .collect::<Vec<_>>();
+    let n_lit = proc_macro2::Literal::usize_unsuffixed(n);
+    quote! {
+        let mut input = String::new();
+        for _ in 0..#n_lit {
+            std::io::stdin().read_line(&mut input).unwrap();
+        }
+        let mut lines = Lines::new(&input);
+        #(#result)*
+    }
+}
+fn consume_lines_from_var_name(
+    fn_sig: &FunctionSignature,
+    var_name: &str,
+) -> proc_macro2::TokenStream {
+    let result = fn_sig
+        .args()
+        .iter()
+        .map(|(name, ty)| {
+            if name.to_string() == var_name {
+                return quote! {
+                   let #name = lines.consume::<usize>().unwrap();
+                   let mut input = String::new();
+                   for _ in 0..#name {
+                       std::io::stdin().read_line(&mut input).unwrap();
+                   }
+                   lines.extend(&input);
+                };
+            }
+            arg_to_consume_line_token_stream(
+                name,
+                ty,
+                &syn::Ident::new("lines", fn_sig.name.span()),
+            )
+        })
+        .collect::<Vec<_>>();
+
+    quote! {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let mut lines = Lines::new(&input);
+
+        #(#result)*
+    }
+}
+
+fn arg_to_consume_line_token_stream(
+    name: &Ident,
+    ty: &Type,
+    lines_ident: &Ident,
+) -> proc_macro2::TokenStream {
+    if is_vec(ty) {
+        let ty = get_vec_type(ty).unwrap();
+        if is_vec(ty) {
+            let ty = get_vec_type(ty).unwrap();
+            return quote! {
+                let #name = #lines_ident.consume_to_two_d_vec::<#ty>().unwrap();
+            };
+        }
+        return quote! {
+            let #name = #lines_ident.consume_to_vec::<#ty>().unwrap();
+        };
+    }
+    quote! {
+        let #name = #lines_ident.consume::<#ty>().unwrap();
+    }
+}
 fn get_vec_type(ty: &Type) -> syn::Result<&Type> {
     let Type::Path(path) = ty else {
         return Err(syn::Error::new(ty.span(), "expected path"));
@@ -72,115 +246,6 @@ fn is_vec(ty: &Type) -> bool {
         }
     }
     false
-}
-
-fn arg_to_consume_line_token_stream(
-    name: &Ident,
-    ty: &Type,
-    lines_ident: &Ident,
-) -> proc_macro2::TokenStream {
-    if is_vec(ty) {
-        let ty = get_vec_type(ty).unwrap();
-        if is_vec(ty) {
-            let ty = get_vec_type(ty).unwrap();
-            return quote! {
-                let #name = #lines_ident.consume_to_two_d_vec::<#ty>().unwrap();
-            };
-        }
-        return quote! {
-            let #name = #lines_ident.consume_to_vec::<#ty>().unwrap();
-        };
-    }
-    quote! {
-        let #name = #lines_ident.consume::<#ty>().unwrap();
-    }
-}
-fn consume_lines(fn_sig: &FunctionSignature, lines_ident: Ident) -> proc_macro2::TokenStream {
-    let result = fn_sig
-        .args
-        .iter()
-        .map(|arg| arg_to_consume_line_token_stream(&arg.0, &arg.1, &lines_ident));
-    // |(name,ty)|{
-    //    if name == row_ref {
-    //       return quote! {
-    //          let #name = #lines_ident.consume::<usize>().unwrap();
-    //          let mut input = String::new();
-    //          for _ in 0..#name {
-    //              std::io::stdin().read_line(&mut input).unwrap();
-    //          }
-    //          let mut lines = #lines_indent.extend(&input);
-    //    }
-    //    let ty = get_vec_type(&ty).unwrap();
-    // }
-
-    quote! {
-        #(#result)*
-    }
-}
-
-fn pte_impl(
-    attr: proc_macro2::TokenStream,
-    item: proc_macro2::TokenStream,
-) -> proc_macro2::TokenStream {
-    let dependencies = dependencies();
-    let fn_sig = fn_parse.parse2(item).unwrap();
-    let consume_lines = consume_lines(&fn_sig, syn::Ident::new("lines", fn_sig.name.span()));
-    let fn_sig_declare = fn_declare(&fn_sig);
-    let fn_sig_execute = fn_execute(&fn_sig);
-
-    let setup_lines = setup_lines(attr);
-    quote! {
-        #dependencies
-
-        #fn_sig_declare
-
-        fn main() {
-            #setup_lines
-            #consume_lines
-            let result = #fn_sig_execute
-            println!("{}", result);
-        }
-    }
-}
-
-fn setup_lines(attr: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
-    let attr_str = attr.to_string();
-    let parse_attr = PteAttrParser::new(&attr_str);
-    if parse_attr.exist_row_num_at_input() {
-        return setup_lines_by_parse_input(parse_attr);
-    }
-    let n = parse_attr.get_row_num().unwrap();
-    setup_lines_by_row_num(n)
-}
-
-fn setup_lines_by_parse_input(parser: PteAttrParser) -> proc_macro2::TokenStream {
-    assert!(parser.exist_row_num_at_input());
-
-    let input_ref = parser.get_input_ref().unwrap();
-    let input_ref: proc_macro2::Literal = syn::parse_str(&input_ref.to_string()).unwrap();
-    quote! {
-        let mut first_line = String::new();
-        std::io::stdin().read_line(&mut first_line).unwrap();
-
-        let row_num = first_line.split_whitespace().nth(#input_ref).unwrap().parse::<usize>().unwrap();
-
-        let mut input = String::new();
-        for _ in 0..row_num {
-            std::io::stdin().read_line(&mut input).unwrap();
-        }
-        let mut lines = Lines::new(&input);
-    }
-}
-
-fn setup_lines_by_row_num(row_num: isize) -> proc_macro2::TokenStream {
-    let row_num_lit = proc_macro2::Literal::isize_unsuffixed(row_num);
-    quote! {
-        let mut input = String::new();
-        for _ in 0..#row_num_lit {
-            std::io::stdin().read_line(&mut input).unwrap();
-        }
-        let mut lines = Lines::new(&input);
-    }
 }
 
 struct FunctionSignature {
@@ -327,134 +392,10 @@ impl PteAttrParser<'_> {
         1
     }
 }
-fn init_consume_line() -> proc_macro2::TokenStream {
-    quote! {
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        let mut lines = Lines::new(&input);
-    }
-}
-fn consume_lines_from_var_name(
-    fn_sig: &FunctionSignature,
-    var_name: &str,
-) -> proc_macro2::TokenStream {
-    let init_consume_line = init_consume_line();
-    let result = fn_sig
-        .args()
-        .iter()
-        .map(|(name, ty)| {
-            if name.to_string() == var_name {
-                return quote! {
-                   let #name = lines.consume::<usize>().unwrap();
-                   let mut input = String::new();
-                   for _ in 0..#name {
-                       std::io::stdin().read_line(&mut input).unwrap();
-                   }
-                   lines.extend(&input);
-                };
-            }
-            arg_to_consume_line_token_stream(
-                name,
-                ty,
-                &syn::Ident::new("lines", fn_sig.name.span()),
-            )
-        })
-        .collect::<Vec<_>>();
-
-    quote! {
-        #init_consume_line
-
-        #(#result)*
-    }
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn consume_line_statement_from_var_name() {
-        let expect = quote! {
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).unwrap();
-            let mut lines = Lines::new(&input);
-
-            let v = lines.consume::<usize>().unwrap();
-            let n = lines.consume::<usize>().unwrap();
-            let mut input = String::new();
-
-            for _ in 0..n {
-                std::io::stdin().read_line(&mut input).unwrap();
-            }
-            lines.extend(&input);
-
-            let vec = lines.consume_to_vec::<usize>().unwrap();
-        };
-        let fn_sig = quote! {
-            fn solve(v: usize,n: usize, vec: Vec<usize>) -> usize {
-                0
-            }
-        };
-        let fn_sig = syn::parse2(fn_sig).unwrap();
-        let got = consume_lines_from_var_name(&fn_sig, "n");
-        assert_eq!(got.to_string(), expect.to_string());
-    }
-    // どうやってlineの記述をするか
-    // どの記述も最後にlinesを作成してあげると、FunctionSignatureのto_consume_lines_token_streamでlinesをconsumeする記述を作成する
-    // ただし、FunctionSignatureはパースされた結果が格納されている構造体であり、その後にプログラムを記述すること自体も責務としてあるが、これはSRPに反している？
-    // FunctionSginatureを単純にデータの提供する構造体として定義して、さまざまなプログラム生成関数に渡すことでプログラムを作成する方が良い気がしてきた
-    // row = 引数の変数名
-    // let mut first_line = String::new();
-    // std::io::stdin().read_line(&mut first_line).unwrap();
-    // let mut lines = Lines::new(&first_line);
-    // let n = lines.consume::<usize>().unwrap();
-    // let mut input = String::new();
-    // for _ in 0..n {
-    //    std::io::stdin().read_line(&mut input).unwrap();
-    // }
-    // let mut lines = Lines::new(&input);
-    // let v = lines.consume::<usize>().unwrap();
-    // row = inNUMBER
-    // let mut first_line = String::new();
-    // std::io::stdin().read_line(&mut first_line).unwrap();
-    // let row_num = first_line.split_whitespace().nth(0).unwrap().parse::<usize>().unwrap();
-    // let mut input = String::new();
-    // for _ in 0..row_num {
-    //     std::io::stdin().read_line(&mut input).unwrap();
-    // }
-    // let mut lines = Lines::new(&input);
-    //
-    // row = NUMBER
-    // let mut input = String::new();
-    // for _ in 0..3 {
-    //     std::io::stdin().read_line(&mut input).unwrap();
-    // }
-    //
-    // let mut lines = Lines::new(&input);
-    // つまり、row = の先はどの記述も必要
-    //
-    //
-    //fn consume_line_statement() {
-    //    let attr = quote! { row = n };
-    //    let item = quote! {
-    //        fn solve(n:usize v: Vec<usize>) -> usize {
-    //            0
-    //        }
-    //    };
-    //    let got = consume_line(attr, item);
-    //    let expect = quote! {
-    //        let mut input1 = String::new();
-    //        std::io::stdin().read_line(&mut input1).unwrap();
-    //        let mut lines = Lines::new(&input1);
-    //        let n = lines.consume::<usize>().unwrap();
-    //        let mut input = String::new();
-    //        for _ in 0..n {
-    //            std::io::stdin().read_line(&mut input).unwrap();
-    //        }
-    //        let mut lines = Lines::new(&input);
-    //        let v = lines.consume::<usize>().unwrap();
-    //    };
-    //    assert_eq!(got.to_string(), expect.to_string());
-    //}
     #[test]
     fn pte_test() {
         let attr = quote! { row = in1 };
@@ -488,23 +429,9 @@ mod tests {
         };
         assert_eq!(got.to_string(), expect.to_string());
     }
+
     #[test]
-    fn setup_lines() {
-        let n = 3;
-        let got = setup_lines_by_row_num(n);
-        let expect = quote! {
-            let mut input = String::new();
-            for _ in 0..3 {
-                std::io::stdin().read_line(&mut input).unwrap();
-            }
-            let mut lines = Lines::new(&input);
-        };
-        assert_eq!(got.to_string(), expect.to_string());
-
-        let attr = "row = in0";
-        let parser = PteAttrParser::new(attr);
-        let got = setup_lines_by_parse_input(parser);
-
+    fn consume_line_statement_from_input() {
         let expect = quote! {
             let mut first_line = String::new();
             std::io::stdin().read_line(&mut first_line).unwrap();
@@ -516,9 +443,68 @@ mod tests {
                 std::io::stdin().read_line(&mut input).unwrap();
             }
             let mut lines = Lines::new(&input);
+
+            let v = lines.consume_to_vec::<usize>().unwrap();
         };
+        let fn_sig = syn::parse2(quote! {
+            fn solve(v:Vec<usize>) -> usize {
+                0
+            }
+        })
+        .unwrap();
+        let got = consume_lines_from_input(&fn_sig, 0);
         assert_eq!(got.to_string(), expect.to_string());
     }
+
+    #[test]
+    fn consume_line_statement_from_row_num() {
+        let expect = quote! {
+            let mut input = String::new();
+            for _ in 0..3 {
+                std::io::stdin().read_line(&mut input).unwrap();
+            }
+            let mut lines = Lines::new(&input);
+            let vec = lines.consume_to_vec::<usize>().unwrap();
+        };
+        let fn_sig = quote! {
+            fn solve(vec: Vec<usize>) -> usize {
+                0
+            }
+        };
+        let fn_sig = syn::parse2(fn_sig).unwrap();
+        let n = 3;
+        let got = consume_lines_from_row_num(&fn_sig, n);
+        assert_eq!(got.to_string(), expect.to_string());
+    }
+
+    #[test]
+    fn consume_line_statement_from_var_name() {
+        let expect = quote! {
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            let mut lines = Lines::new(&input);
+
+            let v = lines.consume::<usize>().unwrap();
+            let n = lines.consume::<usize>().unwrap();
+            let mut input = String::new();
+
+            for _ in 0..n {
+                std::io::stdin().read_line(&mut input).unwrap();
+            }
+            lines.extend(&input);
+
+            let vec = lines.consume_to_vec::<usize>().unwrap();
+        };
+        let fn_sig = quote! {
+            fn solve(v: usize,n: usize, vec: Vec<usize>) -> usize {
+                0
+            }
+        };
+        let fn_sig = syn::parse2(fn_sig).unwrap();
+        let got = consume_lines_from_var_name(&fn_sig, "n");
+        assert_eq!(got.to_string(), expect.to_string());
+    }
+
     #[test]
     fn parse_attr_row_from_input() {
         let attr = "row = in0, column = in1";
